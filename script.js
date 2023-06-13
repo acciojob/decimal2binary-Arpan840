46e39a16e3abd5bb0c62776611c2a7cc333187cd
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-06-13 11:43:47 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function dtoB(num){
+let rems=[];
+while(num>0)
+{
 
-module.exports = threeSum;
+let rem=num%2;
+let cosent=Math.floor(num/2);
+num=cosent;
+rems.push(rem)
+}
+let reverseArray=[]
+ for(let i=rems.length-1;i>=0;i--)
+ {
+    reverseArray.push(rems[i])
+ }
+return reverseArray
+}
+console.log(dtoB(57))
