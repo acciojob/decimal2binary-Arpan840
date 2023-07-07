87a6b9d506529f28fd9aae0e5f472b61d621d393
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:09:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-function dtoB(num){
-let rems=[];
-while(num>0)
-{
+function decimalToBinary(num) {
+  // Write your code here
+}
 
-let rem=num%2;
-let cosent=Math.floor(num/2);
-num=cosent;
-rems.push(rem)
-}
-let reverseArray=[]
- for(let i=rems.length-1;i>=0;i--)
- {
-    reverseArray.push(rems[i])
- }
-return reverseArray
-}
-console.log(dtoB(57))
+window.decimalToBinary = decimalToBinary;
